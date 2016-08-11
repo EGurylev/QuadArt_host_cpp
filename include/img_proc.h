983c8a38f6e9184cb7ae3f200964071e9388896f
Evgeniy <evgeniy.gurilev@gmail.com>
2016-08-11 04:58:17 +0300

    Project files were distributed between src and include folders. Makefile was changed in order to count these changes.

---
+++ include/img_proc.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <QtCore/QObject>
+
+using namespace cv;
+using namespace std;
+using namespace std::chrono;
+
+class img_proc
+{
+	private:
+		const int img_width = 1280;
+		const int img_height = 1024;
+		double area_prev;
+		double perimeter_prev = 160;
+		const double win_scale = 3;
+		double const diff_thresh = 0.2;
+		bool marker_found_prev = false;
+		Point marker_coord;
+		int marker_size;
+		Mat img;
+		void find_marker();
+		void track_marker();
+		void find_corners(vector<int>& x, vector<int>& y, Size frame_size);
+		void mean_shift(Mat frame);
+		void save_img_debug();
+		//debug vars
+		int cnt = 0;
+		int num_debug_img = 50;
+		vector<Mat> img_array;
+        double Duration;
+	public:
+		bool marker_found = false;
+		vector<Point> corner_coord;	
+		img_proc();
+		void marker_search(uint8_t* input_img);
+};
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "control_loop.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    
+    Loop loop_obj;
+    
+    loop_obj.run();
+
+    return a.exec();
+}
+
